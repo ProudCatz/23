9a0e1159570546f08d3bbed5bb9a8ad48978649c
z <z@Z-PC>
2016-04-26 11:52:22 +0800

    00

---
+++ WebApplication2/jq/jQueryHideShow.js
@@ -0,0 +1,40 @@
+﻿/// <reference path="C:\Users\z\documents\visual studio 2015\Projects\WebApplication2\WebApplication2\Scripts/jquery-2.2.3.js" />
+
+
+$(document).ready(
+    function () {
+        $("#btn1").click(function () {
+            $("#p1").hide(1000)
+        });
+    });
+
+$(document).ready(
+    function () {
+        $("#btn2").click(function () {
+            $("#p1").show(1000)
+        });
+    });
+
+$(document).ready(
+    function () {
+        $("#btn3").click(function () {
+            $("#p1").toggle(1000)
+        });
+    });
+
+$(document).ready(
+    function () {
+        $("#p4").click(function () {
+            $("#div1").slideToggle(300)
+        });
+    });
+
+$(document).ready(
+    function () {
+        $("#btn4").click(function () {
+            $("#div2").toggle(300, function () {
+                alert($("#div2").text());
+            })
+        });
+    });
+
---
+++ WebApplication2/jq/jQuerySelecter.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="C:\Users\z\documents\visual studio 2015\Projects\WebApplication2\WebApplication2\Scripts/jquery-2.2.3.js" />
+
+
+$(document).ready(
+    function () {
+        $("#btn1").click(
+            function () {
+                alert($("p").text());
+            });
+
+    });
+$(document).ready(
+function () {
+    $("#btn2").click(
+        function () {
+            alert($("#p1,#p2").text());
+        }
+    )
+});
