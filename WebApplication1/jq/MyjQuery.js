3f8a450d94a832efc8a5db016086cfa13f147003
z <z@Z-PC>
2016-06-17 17:13:01 +0800

    自定义事件,自定义函数

--- WebApplication1/jq/MyjQuery.js
+++ WebApplication1/jq/MyjQuery.js
@@ -1,5 +1,7 @@
 ﻿/// <reference path="C:\Users\z\Documents\Visual Studio 2015\Projects\WebApplication2\WebApplication1\Scripts/jquery-2.2.3.js" />
 
+
+//自定义函数
 $.fn.myadd = function (a, b) {
     b.addClass(a);
 }
@@ -10,3 +12,34 @@ $(document).ready(function () {
     //$("#test").css({ "background": "blue" })
     //$("#btn").addClass("btn btn-success");
 });
+//
+
+
+
+
+
+
+//自定义事件
+$(function () {
+    $("#btn").bind("myClick0", function () {
+        $("#test").append("自定义事件0").append($("<br>"));
+    });
+    $("#btn").bind("myClick1", function () {
+        $("#test").append("自定义事件1").append($("<br>"));
+    });
+    $("#btn").bind("myClick2", function () {
+        $("#test").append("自定义事件2").append($("<br>"));
+    });
+});
+
+$(function () {
+    var i = 0;
+    $("#btn").click(function () {
+        $(this).trigger("myClick" + i);
+        i = i + 1;
+        if (i > 2) {
+            i = 0;
+        }
+    })
+})
+//
